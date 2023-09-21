5ade760cc32a0110d178c796ad3ff6f656db719d
radwan susan <radwansusan90@gmail.com>
2023-09-21 17:45:03 +0400

    Redux Persistance Fix

--- next-food-ordering-app/pages/_app.js
+++ next-food-ordering-app/pages/_app.js
@@ -3,14 +3,36 @@ import '../styles/globals.css';
 import store from '../redux/store';
 import { Provider } from 'react-redux';
 import NextNProgress from 'nextjs-progressbar';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+let persistor = persistStore(store);
+
+// function MyApp({ Component, pageProps }) {
+// 	return (
+// 		<Provider store={store}>
+// 			<Layout>
+// 				<NextNProgress color='#8f0900' />
+// 				<Component {...pageProps} />
+// 			</Layout>
+// 		</Provider>
+// 	);
+// }
+
+// export default MyApp;
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<Provider store={store}>
-			<Layout>
-				<NextNProgress color='#8f0900' />
-				<Component {...pageProps} />
-			</Layout>
+			<PersistGate
+				loading={null}
+				persistor={persistor}
+			>
+				<Layout>
+					<NextNProgress color='#8f0900' />
+					<Component {...pageProps} />
+				</Layout>
+			</PersistGate>
 		</Provider>
 	);
 }
--- next-food-ordering-app/redux/store.js
+++ next-food-ordering-app/redux/store.js
@@ -1,8 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./cartSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './cartSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+const persistConfig = {
+	key: 'root',
+	storage,
+};
+import {
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist';
 
-export default configureStore({
-    reducer: {
-      cart: cartReducer,
-    },
-  });
+const persistedReducer = persistReducer(persistConfig, cartReducer);
+
+const store = configureStore({
+	reducer: {
+		cart: persistedReducer,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
+});
+
+export default store;
