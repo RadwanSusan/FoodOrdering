680d0194921f28e4f32f4d1bcdc8b702a035e9ae
radwan susan <radwansusan90@gmail.com>
2023-09-22 15:21:10 +0400

    Update cart.jsx

--- next-food-ordering-app/pages/cart.jsx
+++ next-food-ordering-app/pages/cart.jsx
@@ -63,7 +63,24 @@ const Cart = () => {
 	};
 
 	const handleRemoveFromCart = (product) => {
-		dispatch(removeFromCart(product.uniqueId));
+		Swal.fire({
+			title: 'Are you sure?',
+			text: "You won't be able to revert this!",
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Yes, delete it!',
+		}).then((result) => {
+			if (result.isConfirmed) {
+				dispatch(removeFromCart(product.uniqueId));
+				Swal.fire(
+					'Deleted!',
+					'Your item has been removed from the cart.',
+					'success',
+				);
+			}
+		});
 	};
 
 	return (
