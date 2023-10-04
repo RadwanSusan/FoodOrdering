016eed66ffb9ab6aa28554977b03eb91226e4a27
radwan susan <radwansusan90@gmail.com>
2023-10-04 16:36:55 +0400

    Update index.jsx

--- next-food-ordering-app/pages/orders/index.jsx
+++ next-food-ordering-app/pages/orders/index.jsx
@@ -5,45 +5,6 @@ import { formatDistanceToNow } from 'date-fns';
 import styles from '../../styles/OrderList.module.css';
 import Link from 'next/link';
 
-// const Orders = () => {
-// 	const dispatch = useDispatch();
-// 	const orders = useSelector(selectOrders);
-// 	console.log(orders);
-
-// 	useEffect(() => {
-// 		fetch(
-// 			`http://localhost:800/api/orders?deviceId=${localStorage.getItem(
-// 				'deviceId',
-// 			)}`,
-// 		)
-// 			.then((response) => response.json())
-// 			.then((data) => dispatch(setOrders(data)));
-// 	}, [dispatch]);
-
-// 	return (
-// 		<div>
-// 			{orders.map((order) => (
-// 				<div key={order._id}>
-// 					<div>
-// 						<img
-// 							src={order.image}
-// 							alt={order.title}
-// 						/>
-// 					</div>
-// 					<h2>{order.title}</h2>
-// 					<p>{order.description}</p>
-// 					<h3>{order.customer}</h3>
-// 					<p>{order.address}</p>
-// 					<p>{order.total}</p>
-// 					<p>{order.method}</p>
-// 					<p>{order.createdAt}</p>
-// 					<p>{order.status}</p>
-// 				</div>
-// 			))}
-// 		</div>
-// 	);
-// };
-
 const tagColors = [
 	'tagTeal',
 	'tagPurple',
@@ -73,33 +34,6 @@ const Orders = () => {
 	};
 	console.log(orders);
 	return (
-		// <div className={styles.container}>
-		// 	{orders.map((order) => (
-		// 		<div
-		// 			className='card'
-		// 			key={order._id}
-		// 		>
-		// 			<div className={styles.cardHeader}>
-		// 				<img
-		// 					src={JSON.parse(orders[0].cart)[0].img}
-		// 					alt={order.title}
-		// 				/>
-		// 			</div>
-		// 			<div className={styles.cardBody}>
-		// 				<h4>{order.title}</h4>
-		// 				<p>{order.description}</p>
-		// 				<div className={styles.user}>
-		// 					<h5>{order.customer}</h5>
-		// 					<p>{order.address}</p>
-		// 					<p>{order.total}</p>
-		// 					<p>{order.method}</p>
-		// 					<p>{order.createdAt}</p>
-		// 					<p>{order.status}</p>
-		// 				</div>
-		// 			</div>
-		// 		</div>
-		// 	))}
-		// </div>
 		<>
 			<h1 style={{ margin: '40px' }}>My Orders</h1>
 			<div className={styles.container}>
