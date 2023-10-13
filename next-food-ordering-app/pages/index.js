7aceab8b96e139681fdaada0312983358e1d0ab0
radwan susan <radwansusan90@gmail.com>
2023-10-13 17:23:33 +0400

    favIcon

--- next-food-ordering-app/pages/index.js
+++ next-food-ordering-app/pages/index.js
@@ -12,16 +12,45 @@ export default function Home({ productsList, admin }) {
 	return (
 		<div>
 			<Head>
-				<title>Pizza Restaurant in Dubai</title>
+				<title>Lahamah And Fahmah</title>
 				<meta
 					name='description'
-					content='Best pizza shop in town'
+					content='Lahamah And Fahmah is a restaurant that serves delicious food'
+				/>
+				<link
+					rel='apple-touch-icon'
+					sizes='180x180'
+					href='/apple-touch-icon.png'
 				/>
 				<link
 					rel='icon'
-					href='/favicon.ico'
-					type='image/x-icon'
+					type='image/png'
+					sizes='32x32'
+					href='/favicon-32x32.png'
 				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='16x16'
+					href='/favicon-16x16.png'
+				/>
+				<link
+					rel='manifest'
+					href='/site.webmanifest'
+				/>
+				<link
+					rel='mask-icon'
+					href='/safari-pinned-tab.svg'
+					color='#5bbad5'
+				/>
+				<meta
+					name='msapplication-TileColor'
+					content='#da532c'
+				/>
+				<meta
+					name='theme-color'
+					content='#ffffff'
+				></meta>
 			</Head>
 			<Featured />
 			{admin ? <AddButton setClose={setClose} /> : null}
