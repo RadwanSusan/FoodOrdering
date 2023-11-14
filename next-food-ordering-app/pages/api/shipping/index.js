4619f8fd827c702f9e2000da85f99d7536ca2ae6
Radwan Susan <radwansusan90@gmail.com>
2023-11-14 18:01:29 +0400

    Update index.js

--- next-food-ordering-app/pages/api/shipping/index.js
+++ next-food-ordering-app/pages/api/shipping/index.js
@@ -1,55 +1,55 @@
-import * as turf from '@turf/turf';
-import polygons from './polygons.json';
+// import * as turf from '@turf/turf';
+// import polygons from './polygons.json';
 
-const downtownPolygon = turf.polygon(polygons.downtownPolygon);
-const BuisnessBayPolygon = turf.polygon(polygons.BuisnessBayPolygon);
+// const downtownPolygon = turf.polygon(polygons.downtownPolygon);
+// const BuisnessBayPolygon = turf.polygon(polygons.BuisnessBayPolygon);
 
-const areaShippingCosts = {
-	downtown: { polygon: downtownPolygon, cost: 30 },
-	BusinessBay: { polygon: BuisnessBayPolygon, cost: 50 },
-};
+// const areaShippingCosts = {
+// 	downtown: { polygon: downtownPolygon, cost: 30 },
+// 	BusinessBay: { polygon: BuisnessBayPolygon, cost: 50 },
+// };
 
-const getShippingCost = (distance) =>
-	isNaN(distance) || distance < 0
-		? 15
-		: distance <= 5
-		? 5
-		: distance <= 10
-		? 10
-		: 20;
+// const getShippingCost = (distance) =>
+// 	isNaN(distance) || distance < 0
+// 		? 15
+// 		: distance <= 5
+// 		? 5
+// 		: distance <= 10
+// 		? 10
+// 		: 20;
 
-export default async function handler(req, res) {
-	const { customerLocation, restaurantAddress } = req.body;
+// export default async function handler(req, res) {
+// 	const { customerLocation, restaurantAddress } = req.body;
 
-	try {
-		const point = turf.point([
-			customerLocation.longitude,
-			customerLocation.latitude,
-		]);
+// 	try {
+// 		const point = turf.point([
+// 			customerLocation.longitude,
+// 			customerLocation.latitude,
+// 		]);
 
-		let shippingCost;
+// 		let shippingCost;
 
-		const area = Object.values(areaShippingCosts).find((area) =>
-			turf.booleanWithin(point, area.polygon),
-		);
+// 		const area = Object.values(areaShippingCosts).find((area) =>
+// 			turf.booleanWithin(point, area.polygon),
+// 		);
 
-		if (area) {
-			shippingCost = area.cost;
-		} else {
-			const from = turf.point([
-				restaurantAddress.longitude,
-				restaurantAddress.latitude,
-			]);
-			const to = point;
-			const options = { units: 'kilometers' };
-			const dist = turf.distance(from, to, options);
-			shippingCost = getShippingCost(dist);
-		}
+// 		if (area) {
+// 			shippingCost = area.cost;
+// 		} else {
+// 			const from = turf.point([
+// 				restaurantAddress.longitude,
+// 				restaurantAddress.latitude,
+// 			]);
+// 			const to = point;
+// 			const options = { units: 'kilometers' };
+// 			const dist = turf.distance(from, to, options);
+// 			shippingCost = getShippingCost(dist);
+// 		}
 
-		res.status(200).json({ shippingCost });
-	} catch (error) {
-		res.status(500).json({
-			error: error.message,
-		});
-	}
-}
+// 		res.status(200).json({ shippingCost });
+// 	} catch (error) {
+// 		res.status(500).json({
+// 			error: error.message,
+// 		});
+// 	}
+// }
